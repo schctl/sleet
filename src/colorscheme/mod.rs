b8c823730705d901d6568ecfeeb97a4e372b3ef2
Sachin Cherian <sachinctl@protonmail.com>
2022-05-27 22:49:46 +0530

    implement catppuccin and nord themes

---
+++ src/colorscheme/mod.rs
@@ -0,0 +1,6 @@
+//! Some color scheme definitions.
+
+#![allow(dead_code)]
+
+pub mod catppuccin;
+pub mod nord;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod color;
 pub mod stylesheet;
 
+#[cfg(feature = "colors-extra")]
+#[rustfmt::skip]
+pub mod colorscheme;
+
 pub use iced_core;
 pub use iced_pure;
 pub use iced_style;
